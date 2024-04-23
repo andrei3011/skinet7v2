85c458296c756e1d7d32ec45177727fa09cba9ab
andrei3011 <p.andreidaniel@yahoo.com>
2024-04-23 12:03:28 +0300

    End of section 5

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,53 @@
+﻿using API.Errors;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BuggyController : BaseApiController
+{
+    private readonly StoreContext _context;
+    public BuggyController(StoreContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("notfound")]
+    public ActionResult GetNotFoundRequest()
+    {
+        return _context.Products.Find(42) == null
+            ? NotFound(new ApiResponse(404)) : Ok();
+    }
+
+
+    [HttpGet("servererror")]
+    public ActionResult GetServerError()
+    {
+        var thing = _context.Products.Find(42);
+
+        var thingToReturn = thing.ToString();
+
+        return Ok();
+    }
+
+
+    [HttpGet("badrequest")]
+    public ActionResult GetBadRequest()
+    {
+        return BadRequest(new ApiResponse(400));
+    }
+
+
+    [HttpGet("badrequest/{id}")]
+    public ActionResult GetBadRequest(int id)
+    {
+        return Ok();
+    }
+
+    [HttpGet("maths/{id}")]
+    public ActionResult GetMath(int id)
+    {
+        var x = 10 / id;
+        return Ok();
+    }
+}
